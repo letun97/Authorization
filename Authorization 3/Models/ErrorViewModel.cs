04a548850241963832e32626ece1aba20af10968
Maxim <letuniz90@gmail.com>
2021-02-10 03:39:21 +0400

    version 1

---
+++ Authorization 3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authorization_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
